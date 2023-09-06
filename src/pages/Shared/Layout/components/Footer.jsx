f272d708995a105714f62a9e58d81c16e3ebd63e
DanScherr <danielscheicher@gmail.comr>
2023-09-06 19:02:18 -0300

    Update Layout

--- src/pages/Shared/Layout/components/Footer.jsx
+++ src/pages/Shared/Layout/components/Footer.jsx
@@ -1,67 +1,33 @@
 //@ts-check
 /** State */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 /** MUI */
 import { 
     Box,
-    BottomNavigation,
-    BottomNavigationAction, 
+    Paper
 } from '@mui/material';
-import RoofingIcon from '@mui/icons-material/Roofing';
-import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
-import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
-import { useNavigate } from 'react-router-dom';
 
 
 export default function SharedLayoutFooter( ) {
-    const [value, setValue] = useState(0);
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        navigateTo(value, navigate);
-    }, [value]);
 
     return(
         <>
             <Box sx={{
                 color: 'primary.lightMain', 
                 bgcolor: 'background.footBox', 
-                boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)'}}
+                boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)',
+                height: '40px',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
             >
-                <BottomNavigation
-                    showLabels
-                    value={value}
-                    onChange={(event, newValue) => {
-                        console.log('mudanca de valor:', value)
-                        setValue(newValue);
-                    }}
-                    sx={{bgcolor: 'background.footBox', }}
-                >
-                    <BottomNavigationAction label="Home" icon={<RoofingIcon />} />
-                    <BottomNavigationAction label="Play" icon={<VideogameAssetIcon />} />
-                    <BottomNavigationAction label="Ranking" icon={<MilitaryTechIcon />} />
-                </BottomNavigation>
+                <Paper sx={{
+                    display: 'block', width: '70px',
+                    height: '35px', lineHeight: '35px',
+                    mx: 'auto', mt: '5px',
+                    textAlign: 'center', fontSize: '18px'
+                }}> 15 </Paper>
             </Box>
         </>
-    )
+    );
 };
-
-const navigateTo = ((valor, func) => {
-    switch (valor) {
-        case 0:
-            func('/');
-            break;
-        
-        case 1:
-            func('/play');
-            break;
-
-        case 2:
-            func('/ranking');
-            break;
-    
-        default:
-            break;
-    };
-});
--- src/pages/Shared/Layout/index.jsx
+++ src/pages/Shared/Layout/index.jsx
@@ -1,12 +1,26 @@
+import { Grid } from "@mui/material";
 import SharedLayoutFooter from "./components/Footer";
 import SharedLayoutHeader from "./components/Header";
 import { Outlet } from "react-router-dom";
+import "./index.css"
 
 export default function SharedLayout(  ) {
     return (
         <div className="container">
             <SharedLayoutHeader />
-            <Outlet />
+            <Grid container columnSpacing={{sm: 2}}
+                sx={{justifyContent: 'center', alignContent: 'center'}}
+            >
+                <Grid xs={0} md={3} className="ShareLayoutGrid SharedLayoutGridRegras">
+                    Regras
+                </Grid>
+                <Grid xs={6} md={6} className="ShareLayoutGrid SharedLayoutGridMain">
+                    <Outlet />
+                </Grid>
+                <Grid xs={0} md={3} className="ShareLayoutGrid SharedLayoutGridBuy">
+                    Buy
+                </Grid>
+            </Grid>
             <div className="fixed-bottom">
                 <SharedLayoutFooter />
             </div>
